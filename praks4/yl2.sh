0ce2c3774f4148d14624fdaa6b0e769505f571e8
Raiko Kyng <raiko.kung@khk.ee>
2019-04-25 15:22:11 +0300

    küpsisetort

--- praks4/yl2.sh
+++ praks4/yl2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+#Skripti algus
 echo "Aluskandiku pikkus cm-tes: "
 read pikkus #kysib sisendit
 echo -n "sisesta aluskandiku laius cm-tes: "
@@ -11,10 +12,16 @@ read kihid
 echo -n "mitu küpsist on pakis "
 read kypsispak
 echo -n "sul on vaja"
+#Arvutamine
+#kypsise pindala
 pindala=`expr $pikkus \* $laius`
+#Kypsisekihtide  kätte saamine kui pindala ja mitu kypsist on teada
 kypsisekiht=`expr $pindala / $kypsis`
+#Kypsisekooki arvutamine kui on teada vaja mitu kihti on vaja
 kypsisekook=`expr $kypsisekiht /* $kihid`
+#Pakkide arvutamine, teada saada  mitu pakki on vaja
 pakid=`expr $kypsiskook \ $kypsispakk`
+#palju küpsiseid alles jääb
 jaak=`expr $kypsiskook % $kypsispakk`
 echo "sul on vaja $pakid pakki küpsiseid ja lisaks $jaak tykki!"
 
