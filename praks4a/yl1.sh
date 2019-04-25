c9607c4c840dceea42a135c8139d99d597a084b2
Raiko Kyng <raiko.kung@khk.ee>
2019-04-25 17:32:49 +0300

    sünniaeg

--- praks4a/yl1.sh
+++ praks4a/yl1.sh
@@ -4,8 +4,9 @@
 echo -n "siseta oma nimi: " #küsib nime
 read nimi
 echo -n "sisesta oma sünniaasta: " #küsib aastat millal sündisid
-read aasta
-vanus=`expr 2019 - $aasta` #arvutab vanuse ära sul
+read synniaasta
+aasta=$(date +'%Y') #hetkese aasta
+vanus=$(expr $((aasta - synniaasta))) #arvutab vanuse ära sul
 echo "$nimi,sina oled $vanus aastat vana" #annab lõpp vastuse
 #
 #skripti lõpp
