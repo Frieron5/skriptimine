ea447889d36a8acb053f847d028fcb8ebd34b2f0
Raiko Kyng <raiko.kung@khk.ee>
2019-04-25 15:25:51 +0300

    Kuupäev

--- praks3/yl1.sh
+++ praks3/yl1.sh
@@ -1,10 +1,12 @@
 #bin/bash
 #
+#Skripti alustamine
+
 #kuupoäeva skript kus näitab kella, kalendrit ja päeva
 #
 echo "Current time" `date +"%A, %B -%d, %Y"` #
 echo "Time"`date +"%H:%M"` #
 cal #
-#
+#Skripti lõpp
 
 
